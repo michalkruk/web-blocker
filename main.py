c54fc759a30635e0c5e2e0ae9420d7d3adafec92
michalkruk <michal.k.kruk@gmail.com>
2019-01-02 14:13:09 +0100

    Fix bug and add instagram to blocked sites

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 import time
 from datetime import datetime as dt
 
-
-
-
-# hosts_path="/etc/hosts"
-
-hosts_temp=r"C:\Users\xarty\OneDrive\Desktop\PythonProjects\WebBlocker\rest"
-hosts_path="hosts"
+hosts_path=r"C:\Windows\System32\drivers\etc"
 redirect="127.0.0.1"
-website_list=["www.facebook.com","facebook.com"]
+website_list=["www.facebook.com","facebook.com","www.facebook.pl","facebook.pl","www.instagram.pl","www.instagram.com"]
 
 while True:
     if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
